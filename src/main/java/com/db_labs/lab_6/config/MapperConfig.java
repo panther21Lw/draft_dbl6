44e80e1506369779db03f3a0879c2440610d39f4
student <oofffmmm@gmail.com>
2025-05-31 02:52:54 +0300

    Add Configuration for ModelMapper

---
+++ src/main/java/com/db_labs/lab_6/config/MapperConfig.java
@@ -0,0 +1,14 @@
+package com.db_labs.lab_6.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
